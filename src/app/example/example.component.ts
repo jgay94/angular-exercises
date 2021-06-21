1929c5efbd5e5ead892b65433349ed73b90c6199
Joseph Gay <49163512+callthecapital@users.noreply.github.com>
2021-06-21 19:01:14 -0400

    add conditionals

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExampleComponent implements OnInit {
   @Input() name: string = "";
+  @Input() age: string = "";
+  @Input() hairColor: string = "";
   constructor() { }
 
   ngOnInit(): void {
