a14c05abe2f1ea81cd2aee86a65b46868de02bb7
Joseph Gay <49163512+callthecapital@users.noreply.github.com>
2021-06-22 14:51:48 -0400

    display lists of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
+import { PeopleListComponent } from './people-list/people-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    PeopleListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people-list',
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.css']
+})
+export class PeopleListComponent implements OnInit {
+  showList: boolean = true;
+  people = [{
+    name: 'John',
+    age: 34,
+    hairColor: 'brown'
+  }, {
+    name: 'Jane',
+    age: 26,
+    hairColor: 'blonde'
+  }, {
+    name: 'Bob',
+    age: 41,
+    hairColor: 'red'
+  }]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
